26a69c5be0a8b8218e53be227dbdaa698f9d1ca6
wky0615 <wky_0615@163.com>
2018-02-01 15:56:37 +0800

    橡皮擦功能

--- script.js
+++ script.js
@@ -1,48 +1,71 @@
 var canvas = document.getElementById('canvas')
-setPage()
 var context = canvas.getContext('2d')
+autoSetPage(canvas)
+listenToMouse(canvas)
 
-window.onresize = function(){
-  setPage()
+var usingEraser = false
+var eraser = document.getElementById('eraser')
+var actions = document.getElementById('actions')
+var pen = document.getElementById('pen')
+eraser.onclick = function () {
+  usingEraser = true
+  actions.className = 'actions switch'
+}
+pen.onclick = function(){
+  usingEraser = false
+  actions.className = 'actions'
 }
 
-var paint = false
-var lastPoint = {
-  x: undefined,
-  y: undefined
-}
-canvas.onmousedown = function (e) {
-  paint = true
-  var x = e.clientX
-  var y = e.clientY
-  drawCircle(x, y, 1)
-  lastPoint.x = x
-  lastPoint.y = y
-}
-
-canvas.onmousemove = function (e) {
-  if (paint) {
+function listenToMouse(canvas) {
+  var active = false
+  var lastPoint = {
+    x: undefined,
+    y: undefined
+  }
+  canvas.onmousedown = function (e) {
     var x = e.clientX
     var y = e.clientY
-    var newPoint = {
-      x: x,
-      y: y
+    active = true
+    if (usingEraser) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      lastPoint.x = x
+      lastPoint.y = y
     }
-    drawCircle(x, y, 1)
-    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-    lastPoint = newPoint
+  }
+
+  canvas.onmousemove = function (e) {
+    var x = e.clientX
+    var y = e.clientY
+    if (!active) { return }
+    if (usingEraser) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      var newPoint = {
+        x: x,
+        y: y
+      }
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
+    }
+  }
+
+  canvas.onmouseup = function (e) {
+    active = false
   }
 }
 
-canvas.onmouseup = function (e) {
-  paint = false
-}
-
-function setPage() {
-  var pageWidth = document.documentElement.clientWidth
-  var pageHeight = document.documentElement.clientHeight
-  canvas.width = pageWidth
-  canvas.height = pageHeight
+function autoSetPage(canvas) {
+  setPage()
+  window.onresize = function () {
+    setPage()
+  }
+  function setPage() {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+    canvas.width = pageWidth
+    canvas.height = pageHeight
+  }
 }
 
 function drawLine(x1, y1, x2, y2) {
@@ -52,10 +75,4 @@ function drawLine(x1, y1, x2, y2) {
   context.lineTo(x2, y2)
   context.stroke()
   context.closePath()
-}
-
-function drawCircle(x, y, radius) {
-  context.beginPath()
-  context.arc(x, y, radius, 0, Math.PI * 2)
-  context.fill()
 }
