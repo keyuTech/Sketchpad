b8e6e662d0a5a0c7efc6e74e80a6ee268670c096
wky0615 <wky_0615@163.com>
2018-02-03 09:30:47 +0800

    添加清空、下载功能，设置背景

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
+context.fillStyle = '#fff'
+context.fillRect(0, 0, canvas.width, canvas.height)
 var lineWidth
 autoSetPage(canvas)
 listenToAction(canvas)
@@ -9,62 +11,83 @@ var pen = document.getElementById('pen')
 var eraser = document.getElementById('eraser')
 var actions = document.getElementById('actions')
 
-pen.onclick = function(){
+pen.onclick = function () {
   usingEraser = false
   pen.classList.add('active')
   eraser.classList.remove('active')
 }
-eraser.onclick = function(){
+eraser.onclick = function () {
   usingEraser = true
   eraser.classList.add('active')
   pen.classList.remove('active')
 }
-black.onclick = function(){
+black.onclick = function () {
   context.strokeStyle = 'rgb(0, 0, 0)'
   black.classList.add('active')
   grey.classList.remove('active')
   brown.classList.remove('active')
   pink.classList.remove('active')
 }
-grey.onclick = function(){
+grey.onclick = function () {
   context.strokeStyle = 'rgb(120, 120, 120)'
   grey.classList.add('active')
   black.classList.remove('active')
   brown.classList.remove('active')
   pink.classList.remove('active')
 }
-brown.onclick = function(){
+brown.onclick = function () {
   context.strokeStyle = 'rgb(152, 0, 48)'
   black.classList.remove('active')
   grey.classList.remove('active')
   brown.classList.add('active')
   pink.classList.remove('active')
 }
-pink.onclick = function(){
+pink.onclick = function () {
   context.strokeStyle = 'rgb(255, 192, 203)'
   black.classList.remove('active')
   grey.classList.remove('active')
   brown.classList.remove('active')
   pink.classList.add('active')
 }
-thin.onclick = function(){
+thin.onclick = function () {
   lineWidth = 2
   thin.classList.add('active')
   normal.classList.remove('active')
   bold.classList.remove('active')
 }
-normal.onclick = function(){
+normal.onclick = function () {
   lineWidth = 5
   normal.classList.add('active')
   thin.classList.remove('active')
   bold.classList.remove('active')
 }
-bold.onclick = function(){
+bold.onclick = function () {
   lineWidth = 8
   bold.classList.add('active')
   thin.classList.remove('active')
   normal.classList.remove('active')
 }
+garbage.onclick = function () {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+}
+download.onclick = function () {
+  //新建一个背景为#fff的画布，画填进去
+  destinationCanvas = document.createElement("canvas");
+  destinationCanvas.width = canvas.width;
+  destinationCanvas.height = canvas.height;
+  destCtx = destinationCanvas.getContext('2d');
+  destCtx.fillStyle = "#FFFFFF";
+  destCtx.fillRect(0, 0, canvas.width, canvas.height);
+  destCtx.drawImage(canvas, 0, 0);
+  var url = destinationCanvas.toDataURL("image/png")
+  var a = document.createElement('a')
+  document.body.appendChild(a)
+  a.href = url
+  a.download = 'image.png'
+  a.target = '_blank'
+  a.click()
+}
+
 
 function listenToAction(canvas) {
   var active = false
